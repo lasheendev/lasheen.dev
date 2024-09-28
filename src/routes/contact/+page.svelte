08ab45b43dfaedf6c41ef87aad6339ae592f5d17
Youssef Lasheen <youssef@lasheen.dev>
2024-09-29 00:28:27 +0300

    Replace contact form with contact links

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,190 +1,103 @@
 <script>
-	import { collection, addDoc, serverTimestamp  } from "firebase/firestore";
-	import { db } from '$lib/firebase';
-	async function onSubmit(e) {
-		await addDoc(collection(db, "Messages"), {
-			name: e.target.Name.value,
-			email: e.target.Email.value,
-			subject: e.target.Subject.value,
-			messageText: e.target.Message.value,
-			timestamp: serverTimestamp(),
-		});
-		e.target.reset();
-	}
+	import Icon from '@iconify/svelte';
+
+	const socialMediaLinks = [
+		{
+			name: 'Facebook',
+			url: 'https://www.facebook.com/lasheen.dev/',
+			icon: 'mdi:facebook',
+			color: '#3b5998'
+		},
+		{
+			name: 'Twitter',
+			url: 'https://x.com/lasheendev',
+			icon: 'fa6-brands:square-x-twitter',
+			color: '#ffffff'
+		},
+		{
+			name: 'Instagram',
+			url: 'https://www.instagram.com/lasheen.dev/',
+			icon: 'ant-design:instagram-filled',
+			color: '#e1306c'
+		},
+		{
+			name: 'LinkedIn',
+			url: 'https://www.linkedin.com/company/lasheendevظ',
+			icon: 'mdi:linkedin',
+			color: '#0077b5'
+		}
+	];
 </script>
 
-<div class="contact">
-	<form on:submit|preventDefault={onSubmit}>
-		<header>
-			<h2>Get in Touch!</h2>
-			<p>We provide a price quote within 2 hours</p>
-		</header>
-		<div class="row">
-			<div class="input-wrapper">
-				<div class="input-wrapper__label">
-					<label for="name" class="input__label">Your name</label>
-				</div>
-				<div class="input-container">
-					<input name="Name" type="text" placeholder="Joe Doe" />
-				</div>
-			</div>
-			<div class="input-wrapper">
-				<div class="input-wrapper__label">
-					<label for="email" class="input__label">Email Address</label>
-				</div>
-				<div class="input-container">
-					<input name="Email" type="email" placeholder="mail@example.com" />
-				</div>
-			</div>
-		</div>
-		<div class="input-wrapper">
-			<div class="input-wrapper__label">
-				<label for="subject" class="input__label">Subject </label>
-			</div>
-			<div class="input-container">
-				<input name="Subject" type="subject" placeholder="I have an amazing app idea" />
-			</div>
-		</div>
-		<div class="input-wrapper">
-			<div class="input-wrapper__label">
-				<label for="name" class="input__label">Message</label>
-			</div>
-			<div class="input-container">
-				<textarea name="Message" placeholder="Body" rows="8" />
-			</div>
-		</div>
+<div id="body">
+	<header>
+		<h2>Get in Touch!</h2>
+		<p>
+			Contact us on social media or via email <a href="mailto::info@lasheen.dev">info@lasheen.dev</a
+			>
+		</p>
+	</header>
 
-		<button type="submit" class="btn">Send Message</button>
-	</form>
+	<div class="button-container">
+		{#each socialMediaLinks as { name, url, icon, color }}
+			<a
+				class="button"
+				href={url}
+				target="_blank"
+				rel="noopener noreferrer"
+				style="background-color: {color}; --color: {color};"
+			>
+				<div id="customicon">
+					<Icon {icon} />
+				</div>
+			</a>
+		{/each}
+	</div>
 </div>
 
 <style lang="scss">
-	.row {
-		flex-direction: row;
+	.button-container {
+		padding-top: 50px;
+		display: grid;
+		gap: 20px;
+		grid-template-columns: 200px 200px 200px 200px;
+		justify-content: center;
+	}
+
+	@media (max-width: $desktop) {
+		.button-container {
+			grid-template-columns: 200px 200px;
+		}
+	}
+
+	@media (max-width: $tablet) {
+		.button-container {
+			grid-template-columns: 200px 200px;
+		}
+	}
+	@media (max-width: $mobile) {
+		.button-container {
+			grid-template-columns: 200px;
+		}
+	}
+
+	.button {
+		aspect-ratio: 1;
 		display: flex;
-	}
-	.btn {
-		transition: all 0.2s cubic-bezier(0.4, 0, 0.2, 1);
-		padding: 0.8rem 1.8rem;
-		border-radius: 0.5rem;
-
-		cursor: pointer;
-
-		color: $clr-fg-2;
-		font-size: 0.8rem;
-		line-height: 1.7;
-		font-weight: bold;
-		text-transform: uppercase;
-		letter-spacing: 0.188em;
-		flex-wrap: nowrap;
-		white-space: nowrap;
-		background: $accent-1;
-		margin-inline: auto;
-		margin-top: 3rem;
-
-		@include hover {
-			background-color: $accent-2;
-		}
+		justify-content: center;
+		align-items: center;
+		font-size: 5rem;
+		border-radius: 10px;
+		text-decoration: none;
+		transition: box-shadow 0.3s ease-in-out;
 	}
 
-	.contact {
-		width: 70%;
-		margin-block: 1rem;
-		padding-inline: 10vw;
+	.button:hover {
+		box-shadow: 0 0 20px var(--color);
 	}
 
-	.input-wrapper {
-		padding-top: 1.5rem;
-		padding-right: 1rem;
-		box-sizing: border-box;
-		width: 100%;
-	}
-
-	h2 {
-		color: $fg;
-
-		font-weight: 500;
-		font-size: 2rem;
-		line-height: 1.7;
-	}
-
-	.contact p {
-		font-family: $body-font;
-		font-weight: 400;
-		line-height: 1.5;
-		color: $fg;
-		letter-spacing: 0.0313em;
-		margin-bottom: 1rem;
-	}
-
-	label {
-		font-family: $body-font;
-		font-size: 10px;
-		line-height: 2.4;
-		color: #bfc0cc;
-		text-transform: uppercase;
-		letter-spacing: 0.125em;
-		user-select: none;
-	}
-
-	input,
-	textarea {
-		color: $fg;
-		font-family: $body-font;
-		font-size: 16px;
-		background: transparentize($clr-bg-350, 0.5);
-		width: 100%;
-		height: 56px;
-		border: 1px solid $accent-3;
-		border-radius: 8px;
-		padding: 16px 56px 16px 16px;
-		transition: box-shadow 0.2s ease-in-out;
-		outline: none;
-	}
-
-	input::placeholder,
-	textarea::placeholder {
-		color: $clr-fg-450;
-	}
-
-	input:-webkit-autofill {
-		-webkit-text-fill-color: $clr-fg-4 !important;
-		-webkit-box-shadow: 0 0 0px 300px #1e1a2c inset !important;
-		border: 1px solid $accent-0 !important;
-	}
-
-	textarea {
-		resize: vertical;
-		height: auto;
-	}
-
-	input:focus,
-	textarea:focus {
-		box-shadow: 0px 0px 0px 2px $clr-fg-5;
-	}
-
-	@media only screen and (max-width: $tablet) {
-		.input-wrapper {
-			padding-top: 1rem;
-		}
-
-		.contact {
-			margin-inline: 0;
-			padding-inline: 3vmin;
-			width: 100%;
-		}
-
-		button {
-			width: 100%;
-		}
-	}
-
-	@media only screen and (max-width: $mobile) {
-		textarea {
-			height: 10rem;
-		}
-		input {
-			height: 2.8rem;
-		}
+	#customicon {
+		display: flex;
+		color: $bg;
 	}
 </style>
