356c9adb27758ab52aac16fe08d2dce272eaaa95
Youssef Lasheen <youssef@lasheen.dev>
2024-09-29 00:58:35 +0300

    Remove firebase remainants

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,5 +2,5 @@
     About
 </h1>
 <p>
-    This website is for lasheen.dev company to show their work and their services. It is a website built with Svelte and firebase.
+    This website is for lasheen.dev company to show their work and their services. It is a website built with Svelte.
 </p>
